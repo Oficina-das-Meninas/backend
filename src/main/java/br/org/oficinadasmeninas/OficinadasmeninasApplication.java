8ef8112bc2186277ed14bc44e2ea0f51108624ab
Giovana Trevizan Barbosa <64981353+giovanatrevizanbarbosa@users.noreply.github.com>
2025-08-28 19:38:09 -0300

    first commit

---
+++ src/main/java/br/org/oficinadasmeninas/OficinadasmeninasApplication.java
@@ -0,0 +1,13 @@
+package br.org.oficinadasmeninas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinadasmeninasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinadasmeninasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/oficinadasmeninas/OficinadasmeninasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.oficinadasmeninas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinadasmeninasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
