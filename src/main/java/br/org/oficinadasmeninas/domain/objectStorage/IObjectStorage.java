bec3c540691d73002930fd6dc07f3e6d3e33e14c
Eduardo Pires Carvalho <epc44129@gmail.com>
2025-09-08 21:04:44 -0300

    refactor: altera campos da interface para definicao de exibicao dos arquivos

--- src/main/java/br/org/oficinadasmeninas/domain/objectStorage/IObjectStorage.java
+++ src/main/java/br/org/oficinadasmeninas/domain/objectStorage/IObjectStorage.java
@@ -2,8 +2,9 @@ package br.org.oficinadasmeninas.domain.objectStorage;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface IObjectStorage {
-    void upload(MultipartFile file);
-    void upload(MultipartFile file, String fileName);
-    MultipartFile getFile(String fileName);
+    void upload(MultipartFile file, Boolean isPublic) throws IOException;
+    void upload(MultipartFile file, String fileName, Boolean isPublic) throws IOException;
 }
