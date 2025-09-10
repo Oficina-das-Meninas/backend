c3c4d0f16556271225219fd1a21d8376d280eb16
roberts <gabrielrobertssoares@gmail.com>
2025-09-10 17:16:00 -0300

    chore: attribute adjustment

--- src/main/java/br/org/oficinadasmeninas/domain/transparency/Category.java
+++ src/main/java/br/org/oficinadasmeninas/domain/transparency/Category.java
@@ -5,6 +5,29 @@ import java.util.UUID;
 public class Category {
     private UUID id;
     private String name;
-    private Boolean isDocument;
+    private Boolean isImage;
     private Integer priority;
+
+    public Category(UUID id, String name, Boolean isImage, Integer priority) {
+        this.id = id;
+        this.name = name;
+        this.isImage = isImage;
+        this.priority = priority;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getIsImage() {
+        return isImage;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
 }
