94cc2b32392dac527271590036a484ea9afd249d
Edenilson Junior <edenilsonju10@gmail.com>
2025-09-11 13:06:17 -0300

    feat: adiciona metodos para buscar entidades de transparencia

--- src/main/java/br/org/oficinadasmeninas/domain/transparency/repository/ITransparencyRepository.java
+++ src/main/java/br/org/oficinadasmeninas/domain/transparency/repository/ITransparencyRepository.java
@@ -1,8 +1,11 @@
 package br.org.oficinadasmeninas.domain.transparency.repository;
 
 import br.org.oficinadasmeninas.domain.transparency.Category;
+import br.org.oficinadasmeninas.domain.transparency.Collaborator;
+import br.org.oficinadasmeninas.domain.transparency.Document;
 import br.org.oficinadasmeninas.domain.transparency.dto.CreateDocumentDto;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +14,10 @@ public interface ITransparencyRepository {
     UUID insertDocument(CreateDocumentDto request);
 
     Optional<Category> findCategoryById(UUID id);
+
+    List<Category> findAllCategories();
+
+    List<Document> findAllDocuments();
+
+    List<Collaborator> findAllCollaborators();
 }
--- src/main/java/br/org/oficinadasmeninas/infra/transparency/repository/TransparencyQueryBuilder.java
+++ src/main/java/br/org/oficinadasmeninas/infra/transparency/repository/TransparencyQueryBuilder.java
@@ -15,4 +15,10 @@ public class TransparencyQueryBuilder {
         FROM categories
         WHERE id = ?
     """;
+
+    public static final String GET_ALL_CATEGORIES = "SELECT id, name, is_image, priority FROM categories";
+
+    public static final String GET_ALL_DOCUMENTS = "SELECT id, title, effective_date, category_id, preview_link FROM documents";
+
+    public static final String GET_ALL_COLLABORATORS = "SELECT id, preview_image_url, name, role, description, priority, category_id FROM collaborators";
 }
