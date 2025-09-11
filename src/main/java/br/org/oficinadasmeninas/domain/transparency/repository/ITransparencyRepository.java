07ac23f5cca0534a918e64d3ef9dee7551cc2761
roberts <gabrielrobertssoares@gmail.com>
2025-09-11 02:12:41 -0300

    feat(category): add repository and service interfaces for category module

---
+++ src/main/java/br/org/oficinadasmeninas/domain/transparency/repository/ITransparencyRepository.java
@@ -0,0 +1,22 @@
+package br.org.oficinadasmeninas.domain.transparency.repository;
+
+import br.org.oficinadasmeninas.domain.transparency.Category;
+import br.org.oficinadasmeninas.domain.transparency.dto.CreateDocumentDto;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ITransparencyRepository {
+
+    UUID insertDocument(CreateDocumentDto request);
+
+    Category insertCategory(Category category);
+    Optional<Category> findCategoryById(UUID id);
+    List<Category> findCategoryAll();
+    Category updateCategory(Category category);
+    void deleteCategory(UUID id);
+    boolean existsById(UUID id);
+    int countDocumentsByCategoryId(UUID id);
+    int countCollaboratorsByCategoryId(UUID id);
+}
---
+++ src/main/java/br/org/oficinadasmeninas/domain/transparency/service/ITransparencyService.java
@@ -0,0 +1,21 @@
+package br.org.oficinadasmeninas.domain.transparency.service;
+
+import br.org.oficinadasmeninas.domain.transparency.dto.CreateCategoryDto;
+import br.org.oficinadasmeninas.domain.transparency.dto.ResponseCategoryDto;
+import br.org.oficinadasmeninas.domain.transparency.dto.UpdateCategoryDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public interface ITransparencyService {
+
+    void uploadDocument(MultipartFile file, String title, Date effectiveDate, String categoryId) throws IOException;
+    ResponseCategoryDto insertCategory(CreateCategoryDto request);
+    ResponseCategoryDto updateCategory(UUID id, UpdateCategoryDto request);
+    void deleteCategory(UUID id);
+    ResponseCategoryDto findCategoryById(UUID id);
+    List<ResponseCategoryDto> findCategoryAll();
+}
