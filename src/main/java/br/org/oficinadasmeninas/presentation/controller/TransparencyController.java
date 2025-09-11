21bd74b83274bbb5edfa64412af577ec10aee112
roberts <gabrielrobertssoares@gmail.com>
2025-09-11 14:10:53 -0300

    chore:remove dependency

--- src/main/java/br/org/oficinadasmeninas/presentation/controller/TransparencyController.java
+++ src/main/java/br/org/oficinadasmeninas/presentation/controller/TransparencyController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
