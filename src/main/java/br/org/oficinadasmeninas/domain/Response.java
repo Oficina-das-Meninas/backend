41c45e9341ff2ba4e4fa0cf5744e23e60fffaa22
Edenilson Junior <edenilsonju10@gmail.com>
2025-10-05 13:58:33 -0300

    feat: ajuste campos Response

--- src/main/java/br/org/oficinadasmeninas/domain/Response.java
+++ src/main/java/br/org/oficinadasmeninas/domain/Response.java
@@ -1,18 +1,33 @@
 package br.org.oficinadasmeninas.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import static java.time.LocalDateTime.now;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response<T> {
 
-    private final Optional<String> message;
+    private final String message;
+
     private final LocalDateTime date = now();
-    private final Optional<T> data;
+    private final T data;
 
     public Response(String message, T data) {
-        this.message = Optional.ofNullable(message);
-        this.data = Optional.ofNullable(data);
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public T getData() {
+        return data;
     }
 }
