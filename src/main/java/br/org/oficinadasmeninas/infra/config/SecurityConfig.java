3034fad690189bfd31d9e2b2a4410fee4a43982b
Edenilson Junior <edenilsonju10@gmail.com>
2025-10-13 21:32:26 -0300

    ajusta cors para aceitar endpoints PATCH

--- src/main/java/br/org/oficinadasmeninas/infra/config/SecurityConfig.java
+++ src/main/java/br/org/oficinadasmeninas/infra/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 
 		configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://apollomusic.com.br", "https://dev.apollomusic.com.br"));
-		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 		configuration.setAllowCredentials(true);
 
