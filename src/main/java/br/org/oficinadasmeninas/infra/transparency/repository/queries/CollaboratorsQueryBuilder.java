37c03b489f4fc5be0d823f5dbe40ac736f80fe58
Edenilson Junior <edenilsonju10@gmail.com>
2025-10-15 22:14:57 -0300

    feat: quebra os metodos do repositorio de transparencia por contexto de entidade envolvida

---
+++ src/main/java/br/org/oficinadasmeninas/infra/transparency/repository/queries/CollaboratorsQueryBuilder.java
@@ -0,0 +1,43 @@
+package br.org.oficinadasmeninas.infra.transparency.repository.queries;
+
+public class CollaboratorsQueryBuilder {
+
+    public static final String INSERT_COLLABORATOR = """
+        INSERT INTO collaborators
+            (id, preview_image_url, category_id, name, role, description, priority)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """;
+
+    public static final String DELETE_COLLABORATOR = """
+        DELETE FROM collaborators
+        WHERE id = ?;
+    """;
+
+    public static final String COUNT_COLLABORATORS_BY_CATEGORY = """
+        SELECT COUNT(*) FROM collaborators WHERE category_id = ?
+    """;
+
+    public static final String GET_COLLABORATOR_BY_ID = """
+             SELECT id, 
+                    preview_image_url, 
+                    category_id, 
+                    name, 
+                    role, 
+                    description, 
+                    priority 
+             FROM collaborators 
+             WHERE id = ?
+            """;
+
+    public static final String GET_ALL_COLLABORATORS = """
+        SELECT id,
+               preview_image_url,
+               name,
+               role,
+               description,
+               priority,
+               category_id
+        FROM collaborators
+    """;
+
+}
