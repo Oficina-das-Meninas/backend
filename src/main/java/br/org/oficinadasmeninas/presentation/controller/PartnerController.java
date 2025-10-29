0b6af770c44c477f0fb959270999aaa3096cceff
Edenilson Junior <edenilsonju10@gmail.com>
2025-10-28 23:21:15 -0300

    feat: adiciona padronizacao de resposta dos endpoints da PartnerController

--- src/main/java/br/org/oficinadasmeninas/presentation/controller/PartnerController.java
+++ src/main/java/br/org/oficinadasmeninas/presentation/controller/PartnerController.java
@@ -1,63 +1,78 @@
 package br.org.oficinadasmeninas.presentation.controller;
 
-import br.org.oficinadasmeninas.domain.partner.Partner;
 import br.org.oficinadasmeninas.domain.partner.dto.CreatePartnerDto;
 import br.org.oficinadasmeninas.domain.partner.dto.UpdatePartnerDto;
-import br.org.oficinadasmeninas.infra.partner.service.PartnerService;
-import br.org.oficinadasmeninas.presentation.shared.PageDTO;
+import br.org.oficinadasmeninas.domain.partner.service.IPartnerService;
+import br.org.oficinadasmeninas.domain.resources.Messages;
 import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @Validated
 @RestController
 @RequestMapping("/api/partners")
-public class PartnerController {
-    private final PartnerService partnerService;
+public class PartnerController extends BaseController {
 
-    public PartnerController(PartnerService partnerService) {
+    private final IPartnerService partnerService;
+
+    public PartnerController(IPartnerService partnerService) {
         this.partnerService = partnerService;
-    }
-
-    @GetMapping
-    public PageDTO<Partner> findAll(@RequestParam @Nullable String searchTerm,
-                                    @RequestParam(defaultValue = "0") @PositiveOrZero int page,
-                                    @RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize){
-        return partnerService.findAll(searchTerm, page, pageSize);
-    }
-
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Partner findPartnerById(@PathVariable UUID id) {
-        return partnerService.findById(id);
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public Partner createPartner(@ModelAttribute @Valid CreatePartnerDto createPartnerDto) throws IOException {
-        return partnerService.createPartner(createPartnerDto);
+    public ResponseEntity<?> createPartner(
+            @ModelAttribute @Valid CreatePartnerDto createPartnerDto
+    ) {
+        return handle(
+                () -> partnerService.insert(createPartnerDto),
+                Messages.PARTNER_CREATED_SUCCESSFULLY,
+                HttpStatus.CREATED
+        );
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Partner updatePartner(
+    public ResponseEntity<?> updatePartner(
             @PathVariable UUID id,
-            @ModelAttribute @Valid UpdatePartnerDto updatePartnerDto) throws Exception {
-
-        return partnerService.updatePartner(id, updatePartnerDto);
+            @ModelAttribute @Valid UpdatePartnerDto updatePartnerDto
+    ) {
+        return handle(
+                () -> partnerService.update(id, updatePartnerDto),
+                Messages.PARTNER_UPDATED_SUCCESSFULLY
+        );
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePartner(@PathVariable UUID id) {
-        partnerService.deletePartner(id);
+    public ResponseEntity<?> deletePartner(@PathVariable UUID id) {
+
+        return handle(
+                () -> partnerService.deleteById(id),
+                Messages.PARTNER_DELETED_SUCCESSFULLY
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll(
+            @RequestParam @Nullable String searchTerm,
+            @RequestParam(defaultValue = "0") @PositiveOrZero int page,
+            @RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize
+    ) {
+        return handle(() -> partnerService.findAll(searchTerm, page, pageSize));
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> findPartnerById(@PathVariable UUID id) {
+        return handle(() -> partnerService.findById(id));
     }
 }
