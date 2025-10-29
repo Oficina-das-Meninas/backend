6fe7e69d31cf13ee44085341d932ba202667c820
Edenilson Junior <edenilsonju10@gmail.com>
2025-10-28 23:21:49 -0300

    feat: adiciona padronizacao de resposta dos endpoints da DonationController

--- src/main/java/br/org/oficinadasmeninas/presentation/controller/DonationController.java
+++ src/main/java/br/org/oficinadasmeninas/presentation/controller/DonationController.java
@@ -1,28 +1,34 @@
 package br.org.oficinadasmeninas.presentation.controller;
 
+import br.org.oficinadasmeninas.application.DonationApplication;
+import br.org.oficinadasmeninas.domain.donation.dto.CreateDonationCheckoutDto;
+import br.org.oficinadasmeninas.domain.resources.Messages;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.org.oficinadasmeninas.application.DonationApplication;
-import br.org.oficinadasmeninas.domain.donation.dto.CreateDonationCheckoutDto;
-import br.org.oficinadasmeninas.domain.donation.dto.DonationCheckoutDto;
-
 @RestController
-@RequestMapping("/api/donation")
-public class DonationController {
+@RequestMapping("/api/donations")
+public class DonationController extends BaseController {
 
-	private final DonationApplication donationApplication;
+    private final DonationApplication donationApplication;
 
-	public DonationController(DonationApplication donationApplication) {
-		this.donationApplication = donationApplication;
-	}
+    public DonationController(DonationApplication donationApplication) {
+        this.donationApplication = donationApplication;
+    }
 
-	@PostMapping("/create")
-	public DonationCheckoutDto createDonationCheckout(@Valid @RequestBody CreateDonationCheckoutDto donationCheckout) {
-		return donationApplication.createDonationCheckout(donationCheckout);
-	}
-	
+    @PostMapping
+    public ResponseEntity<?> createDonationCheckout(
+            @Valid @RequestBody CreateDonationCheckoutDto donationCheckout
+    ) {
+        return handle(
+                () -> donationApplication.createDonationCheckout(donationCheckout),
+                Messages.DONATION_CREATED_SUCCESSFULLY,
+                HttpStatus.CREATED
+        );
+    }
 }
