ff1a4ec4c32aadb7869cdf0853a0cbd20d8235c9
caioslopes <caiolopes.social@gmail.com>
2025-11-23 21:25:37 -0300

    feat: findAllByFilter in admin

--- src/main/java/br/org/oficinadasmeninas/infra/admin/repository/AdminQueryBuilder.java
+++ src/main/java/br/org/oficinadasmeninas/infra/admin/repository/AdminQueryBuilder.java
@@ -2,8 +2,6 @@ package br.org.oficinadasmeninas.infra.admin.repository;
 
 public class AdminQueryBuilder {
 
-    public static final String FIND_ALL_ADMINS = "SELECT id, name, email, password FROM admin";
-
     public static final String FIND_ADMIN_BY_ID = "SELECT id, name, email, password FROM admin WHERE id = ?";
 
     public static final String FIND_ADMIN_BY_EMAIL = "SELECT id, name, email, password FROM admin WHERE email = ?";
@@ -11,4 +9,20 @@ public class AdminQueryBuilder {
     public static final String INSERT_ADMIN = "INSERT INTO admin (id, name, email, password) VALUES (?, ?, ?, ?)";
 
     public static final String UPDATE_ADMIN = "UPDATE admin SET name = ?, email = ?, password = ? WHERE id = ?";
+    
+    public static final String SELECT_COUNT = """
+    		SELECT count(*) FROM admin
+    		WHERE (
+    			unaccent(name) ILIKE unaccent(COALESCE('%' || ? || '%', name))
+    			OR unaccent(email) ILIKE unaccent(COALESCE('%' || ? || '%', email)) 
+    		)
+    		""";
+    
+    public static final String GET_FILTERED_ADMINS = """
+    		SELECT id, name, email, password FROM admin
+    		WHERE (
+    			unaccent(name) ILIKE unaccent(COALESCE('%' || ? || '%', name))
+    			OR unaccent(email) ILIKE unaccent(COALESCE('%' || ? || '%', email)) 
+    		)
+    		""";
 }
