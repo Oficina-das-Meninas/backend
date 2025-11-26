d3122403d23df1701e8e75effd41ae603a2b10d4
Edenilson Junior <edenilsonju10@gmail.com>
2025-11-25 22:45:38 -0300

    refactor: ajusta exceptions para retornar 401 e inversao de logica do if

--- src/main/java/br/org/oficinadasmeninas/infra/account/service/EmailVerificationService.java
+++ src/main/java/br/org/oficinadasmeninas/infra/account/service/EmailVerificationService.java
@@ -1,13 +1,11 @@
 package br.org.oficinadasmeninas.infra.account.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import br.org.oficinadasmeninas.presentation.exceptions.UnauthorizedException;
 import org.springframework.stereotype.Service;
 
 import br.org.oficinadasmeninas.domain.resources.Messages;
-import br.org.oficinadasmeninas.domain.user.dto.UserDto;
 import br.org.oficinadasmeninas.infra.auth.UserDetailsCustom;
 import br.org.oficinadasmeninas.infra.auth.service.JwtService;
-import br.org.oficinadasmeninas.infra.shared.exception.TokenValidationException;
 import br.org.oficinadasmeninas.infra.user.service.UserService;
 
 @Service
@@ -22,26 +20,29 @@ public class EmailVerificationService {
     }
 
     public Void verifyUserEmail(String token) {
-        final String username = jwtService.extractUsername(token);
+        final var username = jwtService.extractUsername(token);
 
         if (username == null) {
-            throw new TokenValidationException(Messages.INVALID_EMAIL_TOKEN);
+            throw new UnauthorizedException(Messages.INVALID_EMAIL_TOKEN);
         }
 
-        UserDto userDto = userService.findByEmail(username);
-        
-        UserDetails userDetails = new UserDetailsCustom(userDto.getId(), userDto.getEmail(), null, userDto.getName(), false);
-        boolean verified = jwtService.isTokenValidForPurpose(
-            token, 
-            userDetails, 
-            JwtService.PurposeTokenEnum.VERIFY_EMAIL
+        var userDto = userService.findByEmail(username);
+
+        if (userDto.isActive())
+            return null;
+
+        var userDetails = new UserDetailsCustom(userDto.getId(), userDto.getEmail(), null, userDto.getName(), false);
+
+        var isTokenValid = jwtService.isTokenValidForPurpose(
+                token,
+                userDetails,
+                JwtService.PurposeTokenEnum.VERIFY_EMAIL
         );
-        
-        if (verified && userDto != null && !userDto.isActive()) {
-        	userService.markUserAsVerified(userDto.getId());
-            return null; 
-        }
-        
-        throw new TokenValidationException(Messages.INVALID_EMAIL_TOKEN);
+
+        if (!isTokenValid)
+            throw new UnauthorizedException(Messages.INVALID_EMAIL_TOKEN);
+
+        userService.markUserAsVerified(userDto.getId());
+        return null;
     }
 }
